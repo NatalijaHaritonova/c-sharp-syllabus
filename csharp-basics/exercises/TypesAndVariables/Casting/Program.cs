628858a8d5f06dd149151bd27caab3e639849feb
NatalijaHaritonova <qvhc@outlook.com>
2021-11-26 22:40:14 +0200

    Casting completed

--- csharp-basics/exercises/TypesAndVariables/Casting/Program.cs
+++ csharp-basics/exercises/TypesAndVariables/Casting/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Casting
 {
@@ -18,27 +14,33 @@ namespace Casting
         static void First()
         {
             string a = "1";
+            int convertedA = int.Parse(a);
             int b = 2;
             int c = 3;
             double d = 4;
+            int convertedD = Convert.ToInt32(d);
             float e = 5;
-
+            int convertedE = (int)e;
             //fixme - should be 15 :|
-            //int sum = a + b + c + d + e;
-            //Console.WriteLine(sum);
+            int sum = convertedA + b + c + convertedD + convertedE;
+            Console.WriteLine(sum);
         }
 
         static void Second()
         {
             string a = "1";
+            float convertedA = float.Parse(a);
             int b = 2;
+            float convertedB = (float)b;
             int c = 3;
+            float convertedC = (float)c;
             double d = 4.2;
+            float convertedD = (float)d;
             float e = 5.3f;
 
             //fixme - should be 15.5 :| 
-            //float sum = a + b + c + d + e;
-            //Console.WriteLine(sum);
+            float sum = convertedA + convertedB + convertedC + convertedD + e;
+            Console.WriteLine(sum);
         }
     }
 }
