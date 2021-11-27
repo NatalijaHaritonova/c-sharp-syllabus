6d626d9cd8086abf3687814f9bc6d8c4e0418436
NatalijaHaritonova <qvhc@outlook.com>
2021-11-27 17:28:33 +0200

    Exercise6 completed

---
+++ csharp-basics/exercises/TypesAndVariables/Exercise6/Program.cs
@@ -0,0 +1,22 @@
+﻿//Write a program that asks the user to enter a series of single digit numbers and calculate the sum of all the numbers.
+namespace Exercise6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter few single digit numbers separated by commas");
+            string inputData;
+            inputData = Console.ReadLine();
+            string[] stringList = inputData.Split(',');
+            int[] intList = Array.ConvertAll(stringList, s => int.Parse(s));
+            int i;
+            int listSum = 0;
+            for (i = 0; i < intList.Length; i++)
+            {
+                listSum += intList[i];
+            }
+            Console.WriteLine(listSum);
+        }
+    }
+}
