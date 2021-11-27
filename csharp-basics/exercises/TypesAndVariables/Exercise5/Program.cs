abb2043e686b8180488d7b285d597365f6ce027d
NatalijaHaritonova <qvhc@outlook.com>
2021-11-27 22:21:14 +0200

    Exercise5 changes

--- csharp-basics/exercises/TypesAndVariables/Exercise5/Program.cs
+++ csharp-basics/exercises/TypesAndVariables/Exercise5/Program.cs
@@ -1,23 +1,4 @@
-﻿/*
-Use several variables to store the names of your classes and their teachers. Then, display a nice little table
-displaying your schedule. FYI, my column of courses has a width of 26 characters, and the teacher column has a
-width of 15. The first and last lines are a plus sign, fifty dashes (a.k.a. minus signs) and another plus sign.
-
-Your table doesn't need to look exactly like this, or even line up.
-+------------------------------------------------------------+
-| 1 |                          English III |       Ms. Lapan |
-| 2 |                          Precalculus |     Mrs. Gideon |
-| 3 |                         Music Theory |       Mr. Davis |
-| 4 |                        Biotechnology |      Ms. Palmer |
-| 5 |           Principles of Technology I |      Ms. Garcia |
-| 6 |                             Latin II |    Mrs. Barnett |
-| 7 |                        AP US History | Ms. Johannessen |
-| 8 | Business Computer Infomation Systems |       Mr. James |
-+------------------------------------------------------------+
-
-
-*/
-namespace Exercise5
+﻿namespace Exercise5
 {
     class Program
     {
