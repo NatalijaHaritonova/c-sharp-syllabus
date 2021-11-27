7a6a36c08439715425eac77e9b87e3224709f9ba
NatalijaHaritonova <qvhc@outlook.com>
2021-11-27 18:04:07 +0200

    Exercise7 completed

---
+++ csharp-basics/exercises/TypesAndVariables/Exercise7/Program.cs
@@ -0,0 +1,24 @@
+﻿//Write a program that prompts the user to enter a string and displays the number of the uppercase letters in the string.
+namespace Exercise7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputData;
+            inputData = Console.ReadLine();
+            char[] charList = inputData.ToCharArray();
+            int i;
+            int count = 0;
+            for (i = 0; i < charList.Length; i++) 
+            {
+                if (char.IsUpper(charList[i]))
+                {
+                    count++;
+                }
+                
+            }
+            Console.WriteLine(count);
+        }
+    }
+}
