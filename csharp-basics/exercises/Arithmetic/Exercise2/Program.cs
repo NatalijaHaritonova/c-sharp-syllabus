72c86b3a3d1f0863feb3a77b94aa9f756330709d
NatalijaHaritonova <qvhc@outlook.com>
2021-11-28 21:44:10 +0200

    Exercise2 completed

---
+++ csharp-basics/exercises/Arithmetic/Exercise2/Program.cs
@@ -0,0 +1,25 @@
+﻿// Write a program called CheckOddEven which prints "Odd Number" if the int variable “number” is odd,
+// or “Even Number” otherwise. The program shall always print “bye!” before exiting.
+namespace Exercise2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CheckOddEven(24);
+            static void CheckOddEven(int num1) 
+            {
+            if (num1 % 2 == 0) 
+                {
+                    Console.WriteLine("Even Number");
+                }
+            else if (num1 % 2 != 0) 
+                {
+                    Console.WriteLine("Odd Number");
+                }
+            }
+            Console.ReadKey();
+            Console.WriteLine("bye!");
+        }
+    }
+}
