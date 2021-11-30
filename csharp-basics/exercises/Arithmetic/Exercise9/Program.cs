320bc4f7d8d79b9337382967b09345f63debb0e5
NatalijaHaritonova <qvhc@outlook.com>
2021-11-30 16:36:39 +0200

    Exercise9 completed

---
+++ csharp-basics/exercises/Arithmetic/Exercise9/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Exercise9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BodyMassIndex(62, 156);
+        }
+
+        private static void BodyMassIndex(double weight, double height)
+        {
+            double poundsCoff = 2.205;
+            double weightP = weight * poundsCoff;
+            double inchCoff = 2.54;
+            double heightI = height / inchCoff;
+            double indexBMI = weightP * 703 / Math.Pow(heightI, 2);
+
+            if (indexBMI > 18.5 && indexBMI < 25)
+            {
+                Console.WriteLine("person's weight is optimal");
+            }
+            else if (indexBMI < 18.5)
+            {
+                Console.WriteLine("person's weight is underweight");
+            }
+            else if (indexBMI > 25)
+            {
+                Console.WriteLine("person's weight is overweight");
+            }
+        }
+    }
+}
