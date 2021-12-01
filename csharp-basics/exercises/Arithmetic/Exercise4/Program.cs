90a40f09f204a218caacbbb768a23c67478d995f
NatalijaHaritonova <qvhc@outlook.com>
2021-12-01 20:43:30 +0200

    Exercise4 changed

--- csharp-basics/exercises/Arithmetic/Exercise4/Program.cs
+++ csharp-basics/exercises/Arithmetic/Exercise4/Program.cs
@@ -8,7 +8,7 @@
             
             static void Product1ToN(int num1)
             {
-            int product = 1;
+                int product = 1;
                 for (int i = num1; i > 0; i--) 
                 {
                     product *= i;
