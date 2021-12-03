bcb761b6d1e174ac44ded52ec03c514af5b426ad
NatalijaHaritonova <qvhc@outlook.com>
2021-12-03 13:29:01 +0200

    Exercise4 completed

---
+++ csharp-basics/exercises/FlowOfControl/FlowControl/Exercise4/Program.cs
@@ -0,0 +1,59 @@
+﻿namespace Exercise4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintDayInWorld();
+
+            static void PrintDayInWorld()
+            {
+                Console.WriteLine("Enter number of day from 0 to 6");
+                var input = Console.ReadLine();
+                int dayNumber;
+                if (int.TryParse(input, out dayNumber))
+                {
+                    if (dayNumber >= 0)
+                    {
+                        switch (dayNumber)
+                        {
+                            case 0:
+                                Console.WriteLine("Sunday");
+                                break;
+                            case 1:
+                                Console.WriteLine("Monday");
+                                break;
+                            case 2:
+                                Console.WriteLine("Tuesday");
+                                break;
+                            case 3:
+                                Console.WriteLine("Wednesday");
+                                break;
+                            case 4:
+                                Console.WriteLine("Thursday");
+                                break;
+                            case 5:
+                                Console.WriteLine("Friday");
+                                break;
+                            case 6:
+                                Console.WriteLine("Saturday");
+                                break;
+                            default:
+                                Console.WriteLine("Not a valid day");
+                                break;
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Not a valid day");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Not a valid day");
+                }
+                   
+            }
+        }
+    }
+}
