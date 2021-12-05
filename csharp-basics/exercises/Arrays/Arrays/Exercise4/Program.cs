7e39a6ba1ada4b182eb79114826ccbf590a191e9
NatalijaHaritonova <qvhc@outlook.com>
2021-12-05 19:17:21 +0200

    Exercise4 completed

--- csharp-basics/exercises/Arrays/Arrays/Exercise4/Program.cs
+++ csharp-basics/exercises/Arrays/Arrays/Exercise4/Program.cs
@@ -1,14 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Exercise4
 {
     class Program
     {
-        //TODO: Write a C# program to test if an array contains a specific value.
         private static void Main(string[] args)
         {
             int[] myArray =
@@ -18,13 +13,27 @@ namespace Exercise4
                 1456, 2265, 1457, 2456
             };
 
-            /*
-            fixme - check if contains number 1245 
-            for (?) {
-                ......
+            var count = 0;
+            int toFind = 1245;
+
+            for (int i = 0; i < myArray.Length; i++) 
+            {
+                if (myArray[i] == toFind)
+                {
+                    count++;
+                }
+            }
+
+            if (count > 0)
+            {
                 Console.WriteLine("Contains!");
             }
-            */
+            else
+            {
+                Console.WriteLine("Do not contain");
+            }
+
+            Console.ReadKey();
         }
     }
 }
