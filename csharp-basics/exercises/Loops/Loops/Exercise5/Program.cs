7e916e4aaf53f61516f6da9e32f45b05fea1eecd
NatalijaHaritonova <qvhc@outlook.com>
2021-12-11 15:17:31 +0200

    Exercise5 completed

---
+++ csharp-basics/exercises/Loops/Loops/Exercise5/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace Exercise5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter first word:");
+            string wOne = Console.ReadLine();
+            Console.WriteLine("Enter second word:");
+            string wTwo = Console.ReadLine();
+            int dots = 30 - wOne.Length - wTwo.Length;
+            Console.Write(wOne);
+            for (int i = 0; i < dots; i++)
+            {
+                Console.Write(".");
+            }
+            Console.Write(wTwo);
+            Console.ReadKey();
+        }
+    }
+}
