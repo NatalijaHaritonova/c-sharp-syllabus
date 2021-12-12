0b80daaee5209099eeb4d8b6b01cf23d980f4625
NatalijaHaritonova <qvhc@outlook.com>
2021-12-12 18:12:16 +0200

    Exercise9 completed

---
+++ csharp-basics/exercises/Loops/Loops/Exercise9/RollTwoDice.cs
@@ -0,0 +1,27 @@
+﻿namespace Exercise9
+{
+    class RollTwoDice
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Desired sum:");
+            int sumD = int.Parse(Console.ReadLine());
+            Random rnd = new Random();
+            for (int i = 0; i < 50; i++)
+            {
+                int dice1 = rnd.Next(1, 7);
+                int dice2 = rnd.Next(1, 7);
+                int realSum = dice1 + dice2;
+                if (sumD == realSum)
+                {
+                    Console.WriteLine(dice1 + " and " + dice2 + " = " + realSum);
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine(dice1 + " and " + dice2 + " = " + realSum);
+                }
+            }
+        }
+    }
+}
