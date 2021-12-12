43dcbd557fb3abbc3668aa97359820a0fddd5c7d
NatalijaHaritonova <qvhc@outlook.com>
2021-12-12 19:18:20 +0200

    Exercise2 changed

--- csharp-basics/exercises/Arrays/Arrays/Exercise2/Program.cs
+++ csharp-basics/exercises/Arrays/Arrays/Exercise2/Program.cs
@@ -10,9 +10,9 @@ namespace Exercise2
             int minNumber = int.Parse(Console.ReadLine());
             Console.WriteLine("Please enter a max number");
             int maxNumber = int.Parse(Console.ReadLine());
-            var sum = minNumber;
+            var sum = 0;
 
-            for (int i = minNumber + 1; i <= maxNumber; i++) 
+            for (int i = minNumber; i <= maxNumber; i++) 
             {
                 sum += i;
             }
