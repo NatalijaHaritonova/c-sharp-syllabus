2773a3838eafd9446b233777a67171208fb3b316
NatalijaHaritonova <qvhc@outlook.com>
2021-12-16 15:43:06 +0200

    Exercise3 completed

---
+++ csharp-basics/exercises/ClassesAndObjects/Exercise3/Odometer.cs
@@ -0,0 +1,43 @@
+﻿namespace Exercise3
+{
+    public class Odometer
+    {
+        private double _mileage;
+        private double _count = 0;
+        private readonly FuelGauge _fuelGauge;
+
+        public Odometer(double kilometers, FuelGauge fuelGauge)
+        {
+            _mileage = kilometers;
+            _fuelGauge = fuelGauge;
+        }
+
+        public double CurrentMileage()
+        {
+            return _mileage;
+        }
+
+        public void Drive()
+        {
+            if (_mileage < 999.999 && _fuelGauge.FuelCheck() == true)
+            {
+                _mileage += 1;
+                _count += 1;
+                
+                if (_count == 10)
+                {
+                    _count -= 10;
+                    _fuelGauge.Decrement();
+                }
+            }
+            else if (_mileage < 999.999 && _fuelGauge.FuelCheck() == false)
+            {
+                Console.WriteLine("Not enough fuel");
+            }
+            else
+            {
+                _mileage = 0;
+            }
+        }
+    }
+}
