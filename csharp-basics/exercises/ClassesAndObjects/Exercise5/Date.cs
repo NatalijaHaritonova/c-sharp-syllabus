6826e84442c5f7fd0f63c00a9f271e46eae915f3
NatalijaHaritonova <qvhc@outlook.com>
2021-12-18 13:19:22 +0200

    Exercise5 completed

---
+++ csharp-basics/exercises/ClassesAndObjects/Exercise5/Date.cs
@@ -0,0 +1,21 @@
+﻿namespace Exercise5
+{
+    internal class Date
+    {
+        public int _year { get; set; }
+        public int _month { get; set; }
+        public int _day { get; set; }
+
+        public Date(int year, int month, int day)
+        {
+            _year = year;
+            _month = month;
+            _day = day;
+        }
+
+        public string DisplayDate()
+        {
+            return $"{_month}/{_day}/{_year}";
+        }
+    }
+}
