87dc4f252874870776aaa59b8b0f7321c0359235
NatalijaHaritonova <qvhc@outlook.com>
2021-12-28 14:54:24 +0200

    VideoStore completed

--- csharp-basics/exercises/ClassesAndObjects/VideoStore/Video.cs
+++ csharp-basics/exercises/ClassesAndObjects/VideoStore/Video.cs
@@ -1,44 +1,74 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace VideoStore
 {
     class Video
     {
+        public double _rating {get; set;} = 0;
+        public string _title { get; set; }
+        public int _countiked { get; set; } = 0;
+        public int _count { get; set; } = 0;
+        public bool _checkedOut { get; set; }
+
         public Video(string title)
         {
-            
+            _title = title;
         }
 
         public void BeingCheckedOut()
         {
-            
+            _checkedOut = true;
         }
 
         public void BeingReturned()
         {
-            
+            _checkedOut = false;
         }
 
-        public void ReceivingRating(double rating)
+        public void ReceivingRating(int rating)
         {
-            
+            _rating += rating;
+            if (rating > 5)
+            {
+                _countiked++;
+                _count++;
+            }
+            else
+            {
+                _count++;
+            }
+            double percentage = _countiked * 100 / _count;
+            if (_countiked > 0)
+            {
+                Console.WriteLine($"{percentage}% liked this movie");
+            }
+            else
+            {
+                Console.WriteLine("No one liked this movie yet");
+            }
         }
 
         public double AverageRating()
         {
-            return 0;
+            return Math.Round(_rating / _count, 1);
         }
 
         public bool Available()
         {
-            return true;
+            if (_checkedOut)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
         }
-
-        public string Title => "";
 
         public override string ToString()
         {
-            return $"{Title} {AverageRating()} {Available()}";
+            return $"{_title}  rating: {AverageRating()} : available : {Available()}";
         }
     }
 }
--- csharp-basics/exercises/ClassesAndObjects/VideoStore/VideoStore.cs
+++ csharp-basics/exercises/ClassesAndObjects/VideoStore/VideoStore.cs
@@ -5,6 +5,10 @@ namespace VideoStore
 {
     class VideoStore
     {
+        private List<string> videoStore = new List<string>();
+        public static Video theMatrix { get; set; } = new Video("The Matrix");
+        public static Video godWars { get; set; } = new Video("Godfather II");
+        public static Video starWars { get; set; } = new Video("Star Wars Episode IV: A New Hope");
 
         public VideoStore()
         {
@@ -13,26 +17,60 @@ namespace VideoStore
 
         public void AddVideo(string title)
         {
-            
+            videoStore.Add(title);
         }
         
         public void Checkout(string title)
         {
-
+            if (ChooseVideo(title)._checkedOut == false)
+            {
+                ChooseVideo(title).BeingCheckedOut();
+            }
+            else
+            {
+                Console.WriteLine("Movie is already rented");
+            }
         }
 
         public void ReturnVideo(string title)
         {
+            if (ChooseVideo(title)._checkedOut == true)
+            {
+                ChooseVideo(title).BeingReturned();
+            }
+            else
+            {
+                Console.WriteLine("Are you sure this movie is from our store?");
+            }
         }
 
-        public void TakeUsersRating(double rating, string title)
+        public void TakeUsersRating(int rating, string title)
         {
-            
+            ChooseVideo(title).ReceivingRating(rating);
         }
 
         public void ListInventory()
         {
-            
+            foreach (string i in videoStore)
+            {
+                Console.WriteLine(ChooseVideo(i).ToString());
+            }
+        }
+
+        public Video ChooseVideo(string title)
+        {
+            if (theMatrix._title == title)
+            {
+                return theMatrix;
+            }
+            else if (godWars._title == title)
+            {
+                return godWars;
+            }
+            else
+            {
+                return starWars;
+            }
         }
     }
 }
