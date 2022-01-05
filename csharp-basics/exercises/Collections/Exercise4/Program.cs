02f6748a5e601b1fdf342159fd8c193d3826915f
NatalijaHaritonova <qvhc@outlook.com>
2022-01-05 21:56:18 +0200

    Exercise4 completed

---
+++ csharp-basics/exercises/Collections/Exercise4/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercise4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number: ");
+            string numbers = Console.ReadLine();
+            int checkN = Convert.ToInt32(numbers);
+            List<int> numInts = new List<int>();
+            int sum = 0;
+
+            FillList(numbers);
+            while(checkN > 1)
+            {
+                CountSum(numInts);
+                if (CheckSum(sum))
+                {
+                    Console.WriteLine(numbers + " is Happy");
+                    checkN = 0;
+                }
+                else
+                {
+                    CheckSumForNum(sum);
+                    ClearList();
+                    FillList(sum.ToString());
+                }
+            }
+
+            void CountSum(List<int> nums)
+            {
+                sum = 0;
+                for (int i = 0; i < nums.Count; i++)
+                {
+                    sum += nums[i] * nums[i];
+                }
+            }
+
+            void FillList(string str)
+            {
+                foreach (char i in str)
+                {
+                    numInts.Add(CharUnicodeInfo.GetDecimalDigitValue(i));
+                }
+            }
+
+            bool CheckSum(int sum)
+            {
+                bool check = false;
+                if (sum == 1)
+                {
+                    check = true;
+                }
+                return check;
+            }
+            void ClearList()
+            {
+                numInts.RemoveRange(0, numInts.Count);
+            }
+
+            void CheckSumForNum(int sum)
+            {
+                if (sum == checkN)
+                {
+                    Console.WriteLine(numbers + " is not happy");
+                    checkN = 0;
+                }
+            }
+        }
+
+    }
+}
