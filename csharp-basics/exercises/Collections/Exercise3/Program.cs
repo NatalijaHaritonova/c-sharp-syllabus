fb112cdc9bcf3336939b5e749344bf6b63aa787d
NatalijaHaritonova <qvhc@outlook.com>
2022-01-05 14:09:29 +0200

    Exercise3 completed

---
+++ csharp-basics/exercises/Collections/Exercise3/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Exercise3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var names = new HashSet<string>();
+            string elem = "new";
+            
+            while (elem.Length > 0)
+            {
+                Console.WriteLine("Enter name: ");
+                elem = Console.ReadLine();
+                names.Add(elem);
+            }
+            Console.Write("Unique name list contains: ");
+            Console.Write(string.Join(" ", names));
+        }
+    }
+}
