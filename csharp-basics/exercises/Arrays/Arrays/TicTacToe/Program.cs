0cb43a91677d172663ea547a827ddf9070d56d61
NatalijaHaritonova <qvhc@outlook.com>
2022-01-12 14:13:43 +0200

    TicTacToe changed

--- csharp-basics/exercises/Arrays/Arrays/TicTacToe/Program.cs
+++ csharp-basics/exercises/Arrays/Arrays/TicTacToe/Program.cs
@@ -7,39 +7,29 @@ namespace TicTacToe
     {
         private static char[,] board = new char[3, 3];
 
-        private static int count = 0;
+        private static int count = 1;
+        private static char xChar = 'X';
+        private static char oChar = 'O';
+        private static char turn;
+        private static bool winCheck = false;
 
         private static void Main(string[] args)
         {
             InitBoard();
             DisplayBoard();
-            int maxRound = 4;
-            int count2 = 0;
+            int maxRound = 10;
 
-            for (int i = 1; i <= maxRound; i++)
+            for (int i = 1; i < maxRound; i++)
             {
-                if (count == 0)
+                Turn();
+                if (count >= 5)
                 {
-                    XTurn();
                     WinTest();
-                    if (count == 0)
+                    if (winCheck)
                     {
-                        OTurn();
-                        WinTest();
-                        count2++;
+                        break;
                     }
                 }
-            }
-
-            if (count == 0 && count2 == 4)
-            {
-                XTurn();
-                WinTest();
-                if (count == 0)
-                {
-                    Console.WriteLine("It's draw!");
-                }
-                Console.ReadKey();
             }
         }
 
@@ -54,6 +44,7 @@ namespace TicTacToe
 
         private static void DisplayBoard()
         {
+            Console.WriteLine("     0|1|2");
             Console.WriteLine("  0  " + board[0, 0] + "|" + board[0, 1] + "|" + board[0, 2]);
             Console.WriteLine("    --+-+--");
             Console.WriteLine("  1  " + board[1, 0] + "|" + board[1, 1] + "|" + board[1, 2]);
@@ -62,54 +53,45 @@ namespace TicTacToe
             Console.WriteLine("    --+-+--");
         }
 
-        private static void XTurn()
+        private static void Turn()
         {
-            char xChar = 'X';
-            Console.WriteLine("'X', Chose your location(row,column):");
-            int[] xMove1 = Console.ReadLine().Split(',').Select(n => Convert.ToInt32(n)).ToArray();
-            board[xMove1[0], xMove1[1]] = xChar;
-            DisplayBoard();
-        }
-
-        private static void OTurn()
-        {
-            char oChar = 'O';
-            Console.WriteLine("'O', Chose your location(row,column):");
-            int[] oMove1 = Console.ReadLine().Split(',').Select(n => Convert.ToInt32(n)).ToArray();
-            board[oMove1[0], oMove1[1]] = oChar;
+            if (count % 2 == 0)
+            {
+                turn = oChar;
+            }
+            else
+            {
+                turn = xChar;
+            }
+            Console.WriteLine($"{turn}, Chose your location(row,column):");
+            int[] move = Console.ReadLine().Split(',').Select(n => Convert.ToInt32(n)).ToArray();
+            board[move[0], move[1]] = turn;
+            count++;
+            Console.Clear();
             DisplayBoard();
         }
         
         private static void WinTest()
         {
-            char xChar = 'X';
-            char oChar = 'O';
-
-            if (board[0, 0].Equals(xChar) && board[0, 1].Equals(xChar) && board[0, 2].Equals(xChar) ||
-                board[1, 0].Equals(xChar) && board[1, 1].Equals(xChar) && board[1, 2].Equals(xChar) ||
-                board[2, 0].Equals(xChar) && board[2, 1].Equals(xChar) && board[2, 2].Equals(xChar) ||
-                board[0, 0].Equals(xChar) && board[1, 0].Equals(xChar) && board[2, 0].Equals(xChar) ||
-                board[0, 1].Equals(xChar) && board[1, 1].Equals(xChar) && board[2, 1].Equals(xChar) ||
-                board[0, 2].Equals(xChar) && board[1, 2].Equals(xChar) && board[2, 2].Equals(xChar) ||
-                board[0, 0].Equals(xChar) && board[1, 1].Equals(xChar) && board[2, 2].Equals(xChar) ||
-                board[2, 0].Equals(xChar) && board[1, 1].Equals(xChar) && board[0, 2].Equals(xChar))
+            if (turn == board[0, 0] && board[0, 0] == board[0, 1] && board[0, 1] == board[0, 2] ||
+                turn == board[1, 0] && board[1, 0] == board[1, 1] && board[1, 1] == board[1, 2] ||
+                turn == board[2, 0] && board[2, 0] == board[2, 1] && board[2, 1] == board[2, 2] ||
+                turn == board[0, 0] && board[0, 0] == board[1, 0] && board[1, 0] == board[2, 0] ||
+                turn == board[0, 1] && board[0, 1] == board[1, 1] && board[1, 1] == board[2, 1] ||
+                turn == board[0, 2] && board[0, 2] == board[1, 2] && board[1, 2] == board[2, 2] ||
+                turn == board[0, 0] && board[0, 0] == board[1, 1] && board[1, 1] == board[2, 2] ||
+                turn == board[2, 0] && board[2, 0] == board[1, 1] && board[1, 1] == board[0, 2])
             {
-                count++;
-                Console.WriteLine($"{xChar} win!");
-                Console.ReadKey();
+                Console.WriteLine($"{turn} win!");
+                winCheck = true;
             }
-            else if (board[0, 0].Equals(oChar) && board[0, 1].Equals(oChar) && board[0, 2].Equals(oChar) ||
-                board[1, 0].Equals(oChar) && board[1, 1].Equals(oChar) && board[1, 2].Equals(oChar) ||
-                board[2, 0].Equals(oChar) && board[2, 1].Equals(oChar) && board[2, 2].Equals(oChar) ||
-                board[0, 0].Equals(oChar) && board[1, 0].Equals(oChar) && board[2, 0].Equals(oChar) ||
-                board[0, 1].Equals(oChar) && board[1, 1].Equals(oChar) && board[2, 1].Equals(oChar) ||
-                board[0, 2].Equals(oChar) && board[1, 2].Equals(oChar) && board[2, 2].Equals(oChar) ||
-                board[0, 0].Equals(oChar) && board[1, 1].Equals(oChar) && board[2, 2].Equals(oChar) ||
-                board[2, 0].Equals(oChar) && board[1, 1].Equals(oChar) && board[0, 2].Equals(oChar))
+            else
             {
-                count++;
-                Console.WriteLine($"{oChar} win!");
-                Console.ReadKey();
+                if (count == 10)
+                {
+                    Console.WriteLine("It's draw!");
+                    Console.ReadKey();
+                }
             }
         }
     }
