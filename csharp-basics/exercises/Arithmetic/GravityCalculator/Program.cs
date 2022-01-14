9a91cbae5dd6ff25257834859c8c65aa115a5dd4
NatalijaHaritonova <qvhc@outlook.com>
2022-01-14 14:17:23 +0200

    GravCalculator done

--- csharp-basics/exercises/Arithmetic/GravityCalculator/Program.cs
+++ csharp-basics/exercises/Arithmetic/GravityCalculator/Program.cs
@@ -6,13 +6,12 @@ namespace GravityCalculator
     {
         static void Main(string[] args)
         {
-            double gravity = -9.81;  // Earth's gravity in m/s^2
+            double gravity = 9.81;  // Earth's gravity in m/s^2
             double initialVelocity = 0.0;
             double fallingTime = 10.0;
             double initialPosition = 0.0;
-            double finalPosition = (0.5 * gravity * Math.Pow(fallingTime, 2) + initialVelocity * fallingTime + initialPosition);
 
-            Console.WriteLine("The object's position after " + fallingTime + " seconds is " + finalPosition + " m.");
+            Console.WriteLine($"The object's position after {fallingTime} seconds is {new Calculator().GravCalculator(gravity, fallingTime, initialVelocity, initialPosition)} m.");
             Console.ReadKey();
         }
     }
