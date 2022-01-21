9256f398afa8bfe96a0dc1b58a248d4f7ac45856
NatalijaHaritonova <qvhc@outlook.com>
2022-01-22 01:11:05 +0200

    WordCountTest done

---
+++ csharp-basics/exercises/Collections/WordCountTests/WCountTests.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace WordCount.Tests
+{
+    [TestClass()]
+    public class WCountTests
+    {
+        private WCount _target;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            _target = new WCount();
+        }
+
+        [TestMethod()]
+        public void LineCountTest()
+        {
+            //Arrange
+            List<string> lines = new List<string> {"Hello hello hello,", "hello, hello, hello"};
+            //Act
+            int result = _target.LineCount(lines);
+            //Assert
+            Assert.AreEqual(2, result);
+        }
+
+        [TestMethod()]
+        public void WordCountTest()
+        {
+            //Arrange
+            List<string> lines = new List<string> { "Hello hello hello,", "hello, hello, hello" };
+            //Act
+            int result = _target.WordCount(lines);
+            //Assert
+            Assert.AreEqual(6, result);
+        }
+
+        [TestMethod()]
+        public void CharCountTest()
+        {
+            //Arrange
+            List<string> lines = new List<string> { "Hello hello hello,", "hello, hello, hello" };
+            //Act
+            int result = _target.CharCount(lines);
+            //Assert
+            Assert.AreEqual(37, result);
+        }
+    }
+}
