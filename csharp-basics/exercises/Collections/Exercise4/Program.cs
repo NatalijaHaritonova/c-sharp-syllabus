76b8cdabb53c23bae9008ba20899631987c06283
NatalijaHaritonova <qvhc@outlook.com>
2022-01-22 17:08:24 +0200

    Exercise4 Collection PR changes

--- csharp-basics/exercises/Collections/Exercise4/Program.cs
+++ csharp-basics/exercises/Collections/Exercise4/Program.cs
@@ -1,23 +1,24 @@
-﻿using System;
-using System.Globalization;
+﻿using System.Globalization;
 
 namespace Exercise4
 {
     class Program
     {
+        private static string numbers;
+        private static int sum = 0;
+        private static int checkN;
+        private static List<int> numInts = new List<int>();
         static void Main(string[] args)
         {
             Console.WriteLine("Enter number: ");
-            string numbers = Console.ReadLine();
-            int checkN = Convert.ToInt32(numbers);
-            List<int> numInts = new List<int>();
-            int sum = 0;
-
+            numbers = Console.ReadLine();
+            checkN = Convert.ToInt32(numbers);
+            
             FillList(numbers);
-            while(checkN > 1)
+            while (checkN > 1)
             {
                 CountSum(numInts);
-                if (CheckSum(sum))
+                if (IsSumOne(sum))
                 {
                     Console.WriteLine(numbers + " is Happy");
                     checkN = 0;
@@ -29,47 +30,41 @@ namespace Exercise4
                     FillList(sum.ToString());
                 }
             }
+        }
 
-            void CountSum(List<int> nums)
+        static void CountSum(List<int> nums)
+        {
+            sum = 0;
+            for (int i = 0; i < nums.Count; i++)
             {
-                sum = 0;
-                for (int i = 0; i < nums.Count; i++)
-                {
-                    sum += nums[i] * nums[i];
-                }
-            }
-
-            void FillList(string str)
-            {
-                foreach (char i in str)
-                {
-                    numInts.Add(CharUnicodeInfo.GetDecimalDigitValue(i));
-                }
-            }
-
-            bool CheckSum(int sum)
-            {
-                bool check = false;
-                if (sum == 1)
-                {
-                    check = true;
-                }
-                return check;
-            }
-            void ClearList()
-            {
-                numInts.RemoveRange(0, numInts.Count);
-            }
-
-            void CheckSumForNum(int sum)
-            {
-                if (sum == checkN)
-                {
-                    Console.WriteLine(numbers + " is not happy");
-                    checkN = 0;
-                }
+                sum += nums[i] * nums[i];
             }
         }
 
+        static void FillList(string str)
+        {
+            foreach (char i in str)
+            {
+                numInts.Add(CharUnicodeInfo.GetDecimalDigitValue(i));
+            }
+        }
+
+        static bool IsSumOne(int sum)
+        {
+            return sum == 1;
+        }
+        static void ClearList()
+        {
+            numInts.RemoveRange(0, numInts.Count);
+        }
+
+        static void CheckSumForNum(int sum)
+        {
+            if (sum == checkN || sum == 4)
+            {
+                Console.WriteLine(numbers + " is not happy");
+                checkN = 0;
+            }
+        }
     }
 }
