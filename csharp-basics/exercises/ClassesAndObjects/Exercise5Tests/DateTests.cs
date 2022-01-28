c1f466d4284b73390a3961b9d91b549b1291bf28
NatalijaHaritonova <qvhc@outlook.com>
2022-01-28 20:34:20 +0200

    ClassesAndObjects Exercise5Tests done

---
+++ csharp-basics/exercises/ClassesAndObjects/Exercise5Tests/DateTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Exercise5.Tests
+{
+    [TestClass()]
+    public class DateTests
+    {
+        private Date _target;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            _target = new Date(2022, 1, 28);
+        }
+        [TestMethod()]
+
+        public void DisplayDateTest()
+        {
+            //Arrange
+            string date = "1/28/2022";
+            //Act
+            var result = _target.DisplayDate();
+            //Assert
+            Assert.AreEqual(date, result);
+        }
+    }
+}
